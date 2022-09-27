8640b27f5742b812274a47c18eb725425bc4675f
MHMD9Khatib <mmkhatib99@gmail.com>
2022-09-27 11:55:48 +0300

    up-1

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const http = require("http");
 const router = require("./router");
 const productRepo = require("./repos/productRepo.js");
-require("env2")('.env');
+require("env2")(".env");
 
 const server = http.createServer(router);
-const port = 5000;
+const port = process.env.PORT;
 
 server.listen(port, () => {
   console.log(
-    `Server is listening on port ${
-      process.env.PORT || 3000
-    }.  Ready to accept requests!`
+    `Server is listening on port ${port || 3000}.  Ready to accept requests!`
   );
 });
 
