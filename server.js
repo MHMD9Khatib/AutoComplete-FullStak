414e228e1ec234c5384fc8e2cbdf7874e0f13088
MHMD9Khatib <mmkhatib99@gmail.com>
2022-09-27 11:15:46 +0300

    last update

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const http = require('http');
-const router = require('./router');
-const productRepo = require('./repos/productRepo.js');
+const http = require("http");
+const router = require("./router");
+const productRepo = require("./repos/productRepo.js");
+require("env2")('.env');
 
 const server = http.createServer(router);
 const port = 5000;
 
-server.listen(port , () =>{
-    console.log(`Server is listening on port ${port}.  Ready to accept requests!`);
-})
+server.listen(port, () => {
+  console.log(
+    `Server is listening on port ${
+      process.env.PORT || 3000
+    }.  Ready to accept requests!`
+  );
+});
 
-
-
-const success = (data) =>{
-    
-    // console.log(data);
+const success = (data) => {
+  // console.log(data);
 };
-const failure = (error) =>{
-    console.log(error);
+const failure = (error) => {
+  console.log(error);
 };
 
-productRepo.getProducts()
-    .then(success)
-    .catch(failure)
+productRepo.getProducts().then(success).catch(failure);
